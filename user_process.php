445160d2fe23b408e05a761263fd982429ac5975
Lucas Campos Naves <95307669+lucasnavex@users.noreply.github.com>
2023-11-24 14:57:26 -0300

    Continuando projeto

---
+++ user_process.php
@@ -0,0 +1,66 @@
+<?php 
+
+    require_once("db.php");
+    require_once("globals.php");
+    require_once("models/User.php");
+    require_once("models/Message.php");
+    require_once("dao/UserDAO.php");
+
+    $message = new Message($BASE_URL);
+
+    $userDao = new UserDAO($conn, $BASE_URL);
+
+    $type = filter_input(INPUT_POST,"type");
+
+    if($type === "update"){
+
+        $userData = $userDao->verifyToken();
+
+        $name = filter_input(INPUT_POST, "name");
+        $lastname = filter_input(INPUT_POST, "lastname");
+        $email = filter_input(INPUT_POST, "email");
+        $bio = filter_input(INPUT_POST, "bio");
+
+        $user = new User();
+
+        $userData->name = $name;
+        $userData->lastname = $lastname;
+        $userData->email = $email;
+        $userData->bio = $bio;
+
+
+        if(isset($_FILES["image"]) && !empty($_FILES["image"]["temp_name"])){
+
+            $image = $_FILES["image"];
+            $imageTypes = ["image/jpeg" , "image/jpg" , "image/png"];
+            $jpgArray = ["image/jpeg" , "image/jpg"];
+            if(in_array($image["type"], $imageTypes)){
+                
+                if(in_array($image, $jpgArray)){
+
+                    $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+
+                } else {
+                    $imageFile = imagecreatefrompng($image["tmp_name"]);
+                }
+
+                $imageName = $user->imageGenerateName();
+
+                imagejpeg($imageFile, "./img/users" . $imageName,)
+
+            }  else {
+                $message->setMessage("Tipo de imagem inválido! insira png ou jpg", "error", "back");
+            }
+
+        }
+
+        $userDao->update($userData);
+
+    } else if($type === "changepassword"){
+
+
+
+    } else {
+
+        $message->setMessage("Informações inválidas!", "error", "back");
+    }
